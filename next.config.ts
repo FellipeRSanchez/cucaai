a286233f7610076bc5b07dfe513152cd479a2eaa
FellipeRSanchez <100930105+FellipeRSanchez@users.noreply.github.com>
2026-05-03 22:13:49 -0300

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ['pdfjs-dist'],
+  serverExternalPackages: ['pdf-parse', 'mammoth'],
 };
 
 export default nextConfig;
